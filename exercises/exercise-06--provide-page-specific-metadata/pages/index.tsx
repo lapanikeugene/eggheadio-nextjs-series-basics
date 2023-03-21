28b3ec6e8eed2b88f609349001dcd76bd0916f52
lapanikeugene <eugeny.lp@gmail.com>
2023-03-21 11:39:37 +0100

    6 done

--- exercises/exercise-06--provide-page-specific-metadata/pages/index.tsx
+++ exercises/exercise-06--provide-page-specific-metadata/pages/index.tsx
@@ -1,10 +1,15 @@
 // ✍️ import Head from 'next/head'
 import { ChakraProvider, Heading } from '@chakra-ui/react'
+import Head from 'next/head'
 
 const Home = () => {
   return (
     <ChakraProvider>
       {/* ✍️ add title and description using the Head component */}
+      <Head>
+        {/* key makes tag unique. other tags with same tags will be discarded */}
+        <title key={'title'}>Title</title>
+      </Head>
       <Heading w="full" textAlign="center">
         This is our homepage content!
       </Heading>
