a13f8782a8315d353ae1c3773585e38ce73a8040
lapanikeugene <eugeny.lp@gmail.com>
2023-03-21 12:11:36 +0100

    9 done

--- exercises/exercise-09--create-custom-style/pages/_app.tsx
+++ exercises/exercise-09--create-custom-style/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from 'next/app'
 
 // ✍️ import the global stylesheet
-
+import '../styles/globals.css'
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  const AnyC = Component as any;
+  return <AnyC {...pageProps} />
 }
 
 export default App
--- exercises/exercise-09--create-custom-style/src/components/button/button.tsx
+++ exercises/exercise-09--create-custom-style/src/components/button/button.tsx
@@ -1,8 +1,8 @@
 // ✍️ import the button's CSS module
-
+import style from './button.module.css';
 const Button = () => {
   // ✍️ set the button's className to the class from the CSS module
-  return <button type="button">Log in</button>
+  return <button type="button" className={style.error} >Log in</button>
 }
 
 export default Button
