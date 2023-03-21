c331f0e396f82a783481927b0e4f6ef0a8843b6d
lapanikeugene <eugeny.lp@gmail.com>
2023-03-21 11:29:35 +0100

    5 done

--- exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
+++ exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
@@ -1,11 +1,17 @@
 // ✍️ import the Image component from 'next/image'
 
 // ✍️ import "lazar.png" statically
-
+import src from '@chakra-ui/icon/dist/declarations/src'
+import Image from 'next/image'
+import lazar from '../lazar.png'
 const Home = () => {
-  return {
+  
     /* ✍️ add the lazar image here */
-  }
+    // image from external url is also possible
+    // but need next.config.js
+    
+   return  (<Image src={lazar} alt={'Lazar'} />)
+  
 }
 
 export default Home
