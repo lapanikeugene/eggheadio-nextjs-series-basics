616dcc92ddcedfd94822a7a226621a0585989839
lapanikeugene <eugeny.lp@gmail.com>
2023-03-21 13:47:14 +0100

    11 done

--- exercises/exercise-11--use-ui-frameworks-like-chakra-ui/pages/_app.tsx
+++ exercises/exercise-11--use-ui-frameworks-like-chakra-ui/pages/_app.tsx
@@ -1,11 +1,18 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import theme from '../theme';
 // ✍️ import the ChakraProvider from '@chakra-ui/react'
 
 // ✍️ import the theme from the theme.ts file
 
 const App = ({ Component, pageProps }: AppProps) => {
   // ✍️ wrap the Component with ChakraProvider and set its theme prop to theme
-  return <Component {...pageProps} />
+  const Comp = Component as any; 
+  return (
+  <ChakraProvider theme={theme}>
+  <Comp {...pageProps} />
+  </ChakraProvider>
+  )
 }
 
 export default App
--- exercises/exercise-11--use-ui-frameworks-like-chakra-ui/pages/_document.tsx
+++ exercises/exercise-11--use-ui-frameworks-like-chakra-ui/pages/_document.tsx
@@ -1,5 +1,7 @@
 // ✍️ import the ColorModeScript from '@chakra-ui/react
+import { ColorModeScript } from '@chakra-ui/react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import theme from '../theme'
 
 // ✍️ import the theme from the theme.ts file
 
@@ -10,6 +12,7 @@ class MyDocument extends Document {
         <Head />
         <body>
           {/* ✍️ add the ColorModeScript and set its initialColorModeProp to theme.config.initialColorMode */}
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
